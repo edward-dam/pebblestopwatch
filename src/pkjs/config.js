c31db824e3447732923165b63bae7e74b53c647a
Edward Dam <edward.dam@gmail.com>
2017-01-29 18:16:08 +0000

    Initial Commit

---
+++ src/pkjs/config.js
@@ -0,0 +1,33 @@
+// Author: Ed Dam
+
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "Stopwatch v1.0"
+  },
+  {
+    "type": "text",
+    "defaultValue": "by Edward Dam"
+  },
+  {
+    "type": "section",
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Stopwatch"
+      },
+      {
+        "type": "text",
+        "defaultValue": "Clock"
+      },
+    ]
+  },
+  {
+    "type": "text",
+    "defaultValue": "Thank you for using my watch app."
+  },
+  {
+    "type": "submit",
+    "defaultValue": "Exit"
+  }
+];
